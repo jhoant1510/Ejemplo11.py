fd5d41c4df5f95bbb27ac46e63b611c997ae2e7b
jhoant1510 <jhoant1510@gmail.com>
2023-04-14 20:06:43 -0500

    primer ejemplo modificacion 3

--- ejemplo11.py
+++ ejemplo11.py
@@ -1,7 +1,1 @@
-print("Hola")
-print(2)
-
-    def __init__(self, nombre, edad, ocupacion):
-        self.edad = edad
-        self.nombre = nombre
-        self.ocupacion = ocupacion 
+print("Hola mundis")
